a987d23a7251d6dc33ccb5f44460ef8d7133885c
Daehwa Seo <sdhwa92@gmail.com>
2020-02-29 17:00:39 +1000

    Added update API

---
+++ update.js
@@ -0,0 +1,35 @@
+import * as dynamoDbLib from "./libs/dynamodb-lib";
+import { success, failure } from "./libs/response-lib";
+
+export async function main(event, context) {
+  const data = JSON.parse(event.body);
+  const params = {
+    TableName: process.env.tableName,
+    // 'Key' defines the partition key and sort key of an item to be updated
+    // - 'userId': Identify Pool identity id of the authenticated user
+    // - 'noteId': path parameter
+    Key: {
+      userId: event.requestContext.identity.cognitoIdentityId,
+      noteId: event.pathParameters.id
+    },
+    // 'UpdateExpression' defines the attributes to be updated
+    // 'ExpressionAttributeValues' defines the value in the update expresssion
+    UpdateExpression: "SET content = :content, attachment = :attachment",
+    ExpressionAttributeValues: {
+      ":attachment": data.attachment || null,
+      ":content": data.content || null
+    },
+    // 'ReturnValues' specifies if and how to return the item's attributes,
+    // where ALL_NEW returns all attributes of the item after the update.
+    ReturnValues: "ALL_NEW"
+  };
+
+  try {
+    var test = await dynamoDbLib.call("update", params);
+    console.log(test);
+    return success({ status: true });
+  } catch (e) {
+    console.log(e);
+    return failure({ status: false});
+  }
+}
