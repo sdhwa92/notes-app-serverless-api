dc12464703ea93b3827c1337d9804c880a8deb2a
Daehwa Seo <sdhwa92@gmail.com>
2020-04-05 16:35:48 +1000

    Revert "Make a mistake"
    This reverts commit 2903ec0b5027d8339ffe459173c05b44a16f200d.

--- create.js
+++ create.js
@@ -3,7 +3,6 @@ import * as dynamoDbLib from "./libs/dynamodb-lib";
 import { success, failure } from "./libs/response-lib";
 
 export async function main(event, context) {
-  uuid.abc.gibberish;
   // Request body is passed in as a JSON encoded string in 'event.body'
   const data = JSON.parse(event.body);
 
